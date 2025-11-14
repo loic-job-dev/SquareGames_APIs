ea34bf10f67bf672a10d53a6f1e19a3a9e61508f
loic jobard <loic-jobard@le-campus-numerique.fr>
2025-11-14 09:35:31 +0100

    first commit

---
+++ src/test/java/fr/campus/squaregamesapi/SquaregamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.campus.squaregamesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquaregamesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
